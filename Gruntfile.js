4e23f944a8d6d2fb56b50ed36d93466b8e56c308
DiegoGervazoni <diegoo.gervazoni@hotmail.com>
2024-02-02 10:17:16 -0300

    Minificação do main.js, config no dist do replace para o js ''enxergar'' o main.min.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,6 +56,10 @@ module.exports = function (grunt) {
               match: "ENDERECO_DO_CSS",
               replacement: "./styles/main.min.css",
             },
+            {
+                match: "ENDERECO_DO_JS",
+                replacement: "./scripts/main.min.js",
+              },
           ],
         },
         files: [
